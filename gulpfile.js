f89cdae46a35810b4b842e604ac0bcd47feefdb7
VladOp02 <152cladvlad228@gmail.com>
2021-12-19 00:35:32 +0300

    files

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
 const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
 const pug = require('gulp-pug');
+const sass = require('gulp-sass');
+const spritesmith = require('gulp.spritesmith');
+const rimraf = require('rimraf');
 
 gulp.task('server', function() {
     browserSync.init({
@@ -22,3 +25,49 @@ gulp.task('templates:compile', function buildHTML() {
     .pipe(gulp.dest('build'))
 
   });
+
+  gulp.task('styles:compile', function () {
+    return gulp.src('source/styles/main.scss')
+      .pipe(sass().on('error', sass.logError))
+      .pipe(gulp.dest('.build/css'));
+  });
+
+  gulp.task('sprite', function (cb) {
+    const spriteData = gulp.src('source/images/icons/*.png').pipe(spritesmith({
+      imgName: 'sprite.png',
+      imgPatch: '../images/sprite.png',
+      cssName: 'sprite.scss'
+    }));
+    spriteData.img.pipe(gulp.dest('build/images/'));
+    spriteData.img.pipe(gulp.dest('source/styles/global/'));
+    cb();
+  });
+
+  gulp.task('clean', function del(cb) {
+      return rimraf('build', cb);
+  })
+
+  gulp.task('copy:fonts', function () {
+    return gulp.src('source/fonts/**/*.*')
+      .pipe(gulp.dest('.build/fonts'));
+  });
+
+  gulp.task('copy:images', function () {
+    return gulp.src('source/images/**/*.*')
+      .pipe(gulp.dest('.build/images'));
+  });
+
+  gulp.task('copy', gulp.parallel('copy:fonts', 'copy:images'));
+
+  gulp.task('watch', function(){
+      gulp.watch('sourse/template/**/*.pug', gulp.series('templates:compile'));
+      gulp.watch('sourse/styles/**/*.scss', gulp.series('styles:compile'));
+  })
+
+
+  gulp.task('default', gulp.series(
+      'clean',
+      gulp.parallel('templates:compile', 'styles:compile', 'sprite', 'copy'),
+      gulp.parallel('watch', 'server')
+    )
+  );
